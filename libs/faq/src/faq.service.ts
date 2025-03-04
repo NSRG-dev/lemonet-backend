1d6b25c4227f994733c3f023659bb98999727faf
idmaksim <dementevmaksim1657@gmail.com>
2025-03-04 22:48:51 +0300

    #7 Настроил кэш TTL

--- libs/faq/src/faq.service.ts
+++ libs/faq/src/faq.service.ts
@@ -1,15 +1,18 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { FaqRepository } from './faq.repository';
 import { I18nService } from 'nestjs-i18n';
 
 @Injectable()
 export class FaqService {
+  private readonly logger = new Logger(FaqService.name);
+
   constructor(
     private readonly faqRepository: FaqRepository,
     private readonly i18n: I18nService,
   ) {}
 
   async findAll() {
+    this.logger.log('Finding all faqs in db');
     return this.faqRepository.findAll();
   }
 
